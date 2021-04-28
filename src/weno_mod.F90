83b632713f7f8fde0cc88dd8509afb06fb30b5cd
Zhou Lilong <dwyanechou@qq.com>
2021-04-28 22:20:02 +0800

    Update weno_mod.F90

--- src/weno_mod.F90
+++ src/weno_mod.F90
@@ -4,7 +4,11 @@
       
     contains
     subroutine calc_weno_coef(coef,stencil_width,nPointsOnEdge,quad_pos)
-      real(r_kind),dimension(:,:) :: coef
+      real   (r_kind), dimension(:,:), intent(out) :: coef
+      integer(i_kind)                , intent(in ) :: stencil_width
+      integer(i_kind)                , intent(in ) :: nPointsOnEdge
+      real   (r_kind), dimension(:,:), intent(in ) :: quad_pos
+      
     end subroutine calc_weno_coef
     
     end module weno_coef_mod
